35a41d4ec060b19e3dd1e26837f0858ad09387a4
William E. Skeith <wes@cs.ccny.cuny.edu>
2026-02-10 11:36:22 -0500

    Broken version of sum.
    Demonstrating scope...

---
+++ 05/sum.cpp
@@ -0,0 +1,26 @@
+/* program to compute the sum of all integers given on stdin */
+#include <iostream>  /* gets definition of cin/cout */
+using namespace std; /* allows us to call them by first name only */
+
+int main()
+{
+	/* need two variables: sum so far and most recent */
+	int recent;  /* most recent number we've heard */
+	int sum = 0; /* sum of all numbers so far */
+	/* step 1 is done... on to step 2: listen for new numbers */
+	while (cin >> recent) {
+		/* NOTE: you can "abuse" cin statements as booleans!  The rule
+		 * is that the cin statement counts as "true" if the read
+		 * was successful and false otherwise. */
+		/* step 3: add both together and write result back to sum */
+		sum = sum+recent;
+		/* equivalent shortcut: */
+		// sum += recent;
+		int x = 99;
+	}
+	// double sum;
+	cout << "sum of all numbers was " << sum << "\n";
+	cout << "x is " << x << "\n";
+}
+
+// vim:foldlevel=3
